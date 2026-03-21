16cc0088938588c92e66dae667511b60feb412cf
Orry Baram <orrybaram@gmail.com>
2026-03-21 01:07:34 -0400

    feat(adr-009): Add GitHub integration section to Integrations page

--- src/components/IntegrationsPage.tsx
+++ src/components/IntegrationsPage.tsx
@@ -1,9 +1,11 @@
+import { GitHubIntegrationSection } from "./GitHubIntegrationSection";
 import { LinearIntegrationSection } from "./LinearIntegrationSection";
 import styles from "./SettingsModal.module.css";
 
 export function IntegrationsPage() {
   return (
     <div className={styles.pageContent}>
+      <GitHubIntegrationSection />
       <LinearIntegrationSection />
     </div>
   );

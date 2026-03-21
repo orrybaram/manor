01bdf7d2671c436f42284b2b63d97f6078f6fd1c
Orry Baram <orrybaram@gmail.com>
2026-03-21 00:58:30 -0400

    feat(adr-010): Create updater module in main process

---
+++ electron/updater.ts
@@ -0,0 +1,44 @@
+import { BrowserWindow } from "electron";
+import { autoUpdater, type UpdateInfo, type ProgressInfo } from "electron-updater";
+
+export function initAutoUpdater(win: BrowserWindow): void {
+  autoUpdater.autoDownload = true;
+  autoUpdater.autoInstallOnAppQuit = true;
+
+  autoUpdater.on("update-available", (info: UpdateInfo) => {
+    win.webContents.send("updater:update-available", info);
+  });
+
+  autoUpdater.on("update-downloaded", (info: UpdateInfo) => {
+    win.webContents.send("updater:update-downloaded", info);
+  });
+
+  autoUpdater.on("error", (err: Error) => {
+    win.webContents.send("updater:error", err.message);
+  });
+
+  autoUpdater.on("download-progress", (progress: ProgressInfo) => {
+    win.webContents.send("updater:download-progress", {
+      percent: progress.percent,
+      bytesPerSecond: progress.bytesPerSecond,
+      transferred: progress.transferred,
+      total: progress.total,
+    });
+  });
+
+  setTimeout(() => {
+    try {
+      autoUpdater.checkForUpdates();
+    } catch {
+      // In dev mode or without code signing, this will fail silently
+    }
+  }, 5000);
+}
+
+export function checkForUpdates(): void {
+  autoUpdater.checkForUpdates();
+}
+
+export function quitAndInstall(): void {
+  autoUpdater.quitAndInstall();
+}
